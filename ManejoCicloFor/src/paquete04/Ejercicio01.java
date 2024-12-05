90105c74e6340d0136a6886988c18326bce92d9d
Daniel Idrovo <doidrodo@utpl.edu.ec>
2024-12-05 12:57:06 -0500

    cambios2

--- ManejoCicloFor/src/paquete04/Ejercicio01.java
+++ ManejoCicloFor/src/paquete04/Ejercicio01.java
@@ -11,20 +11,27 @@ package paquete04;
 public class Ejercicio01 {
     public static void main(String[] args) {
         int num1;
-        int num2;
-        int num3;
-        int num4;
+        int o2;
+        int o3;
+        int o4;
         int limite;
         String cadena1 = "";
         
         for(num1 = 30; num1 >= 10; num1--){
-            limite = num1;
-            for(num2 = 60; num2 >=20; num2 -= 2 ){
-            cadena1 = String.format("%s %d - %d - \n",
-                    cadena1, num1, num2);
+           
+            o2 = num1 * 2;
+            o3 = num1 * 3;
+            o4 = num1 * 4;
+            
+                   
+            cadena1 = String.format("%s %d - %d - %d - %d\n",
+                    cadena1, num1, o2, o3, o4);
+               }
+        System.out.printf("%s\n", cadena1);
             }
+    
         }
                 
-        System.out.printf("%s\n", cadena1);
-    }
-}
+        
+    
+
